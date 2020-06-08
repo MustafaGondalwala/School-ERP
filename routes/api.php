ee97ed7a931ccb6547a154c3bae283b0f6c21780
Ankit Kumar <ankitkumarraj349@gmail.com>
2020-06-08 16:08:47 +0530

    opcache enabled

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 
 Route::post('/v1/login',"UserApiController@login");
 Route::group(["prefix"=>"v1","middleware" => "auth:api"],function(){
