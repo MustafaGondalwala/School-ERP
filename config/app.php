20e1e1ae647b6147bb0abbca2fffb4491b5807e3
Ankit Kumar <ankitkumarraj349@gmail.com>
2020-06-08 16:32:02 +0530

    opcache

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => 'http://insta-develop-sms.herokuapp.com/',
+    'url' => env('APP_URL', 'http://insta-develop-sms.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
